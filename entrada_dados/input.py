56dde4ff006247def91aa642d705657fce5b1e6a
laura <laurabeatriz.alexandre0901@gmail.com>
2026-08-05 16:00:23 -0300

    funcoes

---
+++ entrada_dados/input.py
@@ -0,0 +1,14 @@
+nome = input("Digite seu nome: ")
+print(f"Bem vindo {nome}!")
+
+idade = int(input("Digite sua idade: "))
+print(f"Sua idade: {idade}")
+
+idade_mais_dez = idade + 10
+print(f"Sua idade: {idade_mais_dez}")
+
+prin(type(nome))
+print(type(idade))
+
+valor_do_produto = float(input("Digite o valor do produto: "))
+print(f"O valor do produto: {valor_do_produto}")
